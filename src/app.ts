48dab42bb833e4d82c5a472d4a718e3f944a8eac
gusants <gs17047080@gmail.com>
2022-07-14 12:43:25 -0300

    Feat: Created routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,15 @@
 import express from "express";
 import "reflect-metadata";
+import routesADM from "./routers/admin";
+import routesClients from "./routers/clients";
+import routesEmployee from "./routers/employee";
 
 const app = express();
 app.use(express.json());
+
+app.use("/hotel", routesADM, routesEmployee);
+app.use("/service", routesEmployee);
+app.use("/clients", routesClients);
 
 app.listen(3000, () => {
   console.log("App runing");
---
+++ src/routers/admin/index.ts
@@ -0,0 +1,48 @@
+import { Router } from "express";
+
+const routesADM = Router();
+
+// Listagem e Criação de hotéis no banco de dados
+routesADM.post("" /*Controller de criação do hotel*/);
+routesADM.get("" /*Controller de listagem dos hotéis*/);
+
+//Alterações de dados do hotel no banco de dados
+routesADM.patch("/:id" /*Controller de alteração de dados do hotel*/);
+routesADM.delete("/:id" /*Controller de deleção do hotel*/);
+routesADM.get("/:id" /*Controller de listagem do hotel*/);
+
+//Criação e listagem de cargos
+routesADM.post("/:id/jobtitles" /*Controller de criação de cargos do hotel*/);
+routesADM.get("/:id/jobtitles" /*Controller de listagem dos cargos do hotel*/);
+
+//Alterações de dados nos cargos
+routesADM.get(
+  "/:id/jobtitles/:id" /*Controller de listagem do cargo do hotel*/
+);
+routesADM.patch(
+  "/:id/jobtitles/:id" /*Controller de alteração de dados do cargo no hotel*/
+);
+routesADM.delete(
+  "/:id/jobtitles/:id" /*Controller de deleção de dados do cargo no hotel*/
+);
+
+//Criação e listagem dos quartos do hotel
+routesADM.get("/:id/rooms" /*Controller de listagem dos quartos do hotel*/);
+routesADM.post("/:id/rooms" /*Controller de criação do quarto do hotel*/);
+
+//Alterações de dados dos quartos
+routesADM.get("/:id/rooms/:id" /*Controller de listagem do quarto do hotel*/);
+routesADM.patch(
+  "/:id/rooms/:id" /*Controller de alterações de dados do quarto do hotel*/
+);
+routesADM.delete("/:id/rooms/:id" /*Controller de deleção do quarto do hotel*/);
+
+//Criação de funcionários
+routesADM.post(
+  "/:id/employees" /*Controller de criação dos funcionários do hotel*/
+);
+routesADM.get(
+  "/:id/employees" /*Controller de listagem dos funcionários do hotel*/
+);
+
+export default routesADM;
