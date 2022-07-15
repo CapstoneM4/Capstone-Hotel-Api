ce8520459109f537a476b5a25028e48a8e0f0c75
kennedybm <kennedy_muniz@hotmail.com>
2022-07-15 15:16:17 -0300

    Config prod

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import "reflect-metadata";
 import "express-async-errors";
-import isAdmUserMiddleware from "./middlewares/handdleError.middleware";
 import routesADM from "./routers/admin/admin.routes";
 import routesClients from "./routers/clients/clients.routes";
 import routesEmployee from "./routers/employee/employee.routes";
+import handleAppErrorMiddleware from "./middlewares/handdleError.middleware";
 
 const app = express();
 app.use(express.json());
@@ -12,8 +12,10 @@ app.use(express.json());
 app.use("/hotel", routesADM, routesEmployee);
 app.use("/service", routesEmployee);
 app.use("/clients", routesClients);
-app.use(isAdmUserMiddleware);
-app.listen(3000, () => {
+
+app.use(handleAppErrorMiddleware);
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("App runing");
 });
 export default app;
