16e445fd78fb760324bb7f100e2c86542b7e7986
kennedybm <kennedy_muniz@hotmail.com>
2022-07-20 19:55:35 -0300

    Config Prod

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import routesADM from "./routers/admin/admin.routes";
 import routesClients from "./routers/clients/clients.routes";
 import routesEmployee from "./routers/employee/employee.routes";
 import handleAppErrorMiddleware from "./middlewares/handdleError.middleware";
+import { env } from "process";
 
 const app = express();
 app.use(express.json());
@@ -13,7 +14,7 @@ app.use("/service", routesEmployee);
 app.use("/clients", routesClients);
 app.use(handleAppErrorMiddleware);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("App runing");
 });
 export default app;
